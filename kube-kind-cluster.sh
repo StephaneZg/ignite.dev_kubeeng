6fba52e8d83170a11645be6f827df86e79be4074
StephaneZg <stephane.zangbengono@gmail.com>
2024-02-15 14:41:40 +0000

    Added docker engine installation

--- kube-kind-cluster.sh
+++ kube-kind-cluster.sh
@@ -4,6 +4,23 @@ sudo apt-get update -y
 
 architecture=$(uname -m)
 
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+# Install Docker Engine
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin
+
 # Download the release base on the system architecture
 if [ "$architecture" = "x86_64" ]; then
         curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.22.0/kind-linux-amd64
